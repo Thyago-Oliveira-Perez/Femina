cacfb04ce783faa95b229483a48ff56126eef7df
anna sganderla <sganderlakarlaanna@gmail.com>
2021-10-21 14:39:03 -0300

    add new header

--- main.js
+++ main.js
@@ -12,3 +12,25 @@ const modal = {
       .classList.remove('active')  // remove o active
     }
 }
+
+
+const [scrollX, setScrollX] = useState(0);
+
+    const leftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const rightArrow = () => {
+      let x = scrollX - Math.round(window.innerWidth / 2);
+      let listW = items.results.length * 150; // largura da lista inteira
+
+      if ((window.innerWidth - listW) > x) {
+          x = (window.innerWidth - listW) - 60;
+      }
+      
+      setScrollX(x);
+  }
